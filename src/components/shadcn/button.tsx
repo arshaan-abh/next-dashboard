dc8af5eaffd70b36fdcd94aa6939aef65b956868
Arshaan <arshaan.abh@gmail.com>
2025-05-10 12:55:04 +0330

    Fixed Shadcn's button a bit

--- src/components/shadcn/button.tsx
+++ src/components/shadcn/button.tsx
@@ -23,7 +23,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5",
+        sm: "h-8 gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 px-6 has-[>svg]:px-4",
         xl: "h-11 px-8 has-[>svg]:px-4",
         icon: "size-9",
@@ -36,6 +36,7 @@ const buttonVariants = cva(
     defaultVariants: {
       variant: "default",
       size: "default",
+      rounded: "default",
     },
   },
 );
